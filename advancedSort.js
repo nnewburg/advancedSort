48a42e914fbc4866cb90ab44f63188ec524298e0
Nick <pickme1152019@gmail.com>
2019-03-07 21:30:37 +0000

    created two sorting methods

--- advancedSort.js
+++ advancedSort.js
@@ -4,3 +4,25 @@ var students = [
   { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
 ];
+
+function advancedSort(data){
+
+let sortName = data.sort(function(a, b){
+  let nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  if (nameA == nameB )
+  return b.age - a.age;
+});
+
+return sortName;
+
+}
+
+console.log(advancedSort(students));
+
